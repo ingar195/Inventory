38cbaea96a2e513a98dccb257ea011816d7776b5
Ingar <ingar@osc.no>
2019-08-21 20:30:44 +0200

    added functions

--- Inventory.py
+++ Inventory.py
@@ -16,24 +16,40 @@ def initdb():
     minstock VARCHAR(255), 
     barcode VARCHAR(255), 
     checkedoutby VARCHAR(255), 
-    checkedoutdate VARCHAR(255));"""
+    checkedoutdate VARCHAR(255),
+    category VARCHAR(255);"""
 
     cursor.execute(sql_command)
     connection.commit()
+def checkifexist(dbname, query):
+    print("todo")
+
+def search(db, query):
+    print("todo")
 
 
-def appeddb(table, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate):
-    sql_command = """INSERT INTO {} (itemnumber, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate) VALUES (null,"{}","{}","{}","{}","{}","{}","{}","{}");"""
-    print(" debug: " + sql_command.format(table, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate))
-    cursor.execute(sql_command.format(table, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate))
+def delete(db, itemnumber):
+    sql_command = """DELETE FROM {} WHERE itemnumber = {};"""
+    cursor.execute(sql_command.format(db, itemnumber))
+    connection.commit()
+
+
+def appeddb(table, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate, category):
+    # Check if exist func
+    sql_command = """INSERT INTO {} (itemnumber, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate, category) VALUES (null,"{}","{}","{}","{}","{}","{}","{}","{}","{}");"""
+    print(" debug: " + sql_command.format(table, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate, category))
+    cursor.execute(sql_command.format(table, name, description, storelocation, stock, minstock, barcode, checkedoutby, checkedoutdate, category))
 
     connection.commit()
 
 
-
-
-#initdb()
+# initdb()
 print("123")
-appeddb("Inventory", "Hammer", "Bighammer", "WorkShop_A3", "1", "0", "5000112637397", "Ingar", "2019-08-21")
+
+# appeddb("Inventory", "Hammer", "Bighammer", "WorkShop_A3", "1", "0", "5000112637397", "Ingar", "2019-08-21", "Hand tools") # cola
+# appeddb("Inventory", "Helmet", "protective helmet", "WorkShop_P1", "5", "2", "7025150014106", "Ingar", "2019-08-21", "Hand tools") #eplejuce
+# appeddb("Inventory", "M5", "M5 hex 25mm", "WorkShop_S1", "73", "10", "", "", "2019-08-21", "Skrews")
+
+delete("Inventory", 5)
 print("321")
 connection.close()
